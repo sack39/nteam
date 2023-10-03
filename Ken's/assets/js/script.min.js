d2ed3e65a40e1c5e11adb5a711d96e538137f1c3
sack39 <mementosakuma@gmail.com>
2023-10-03 12:18:57 +0900

    add Ken's

---
+++ Ken's/assets/js/script.min.js
@@ -0,0 +1,1 @@
+"use strict";document.addEventListener("DOMContentLoaded",function(){let n=window.innerWidth<=768;window.addEventListener("resize",function(){n=window.innerWidth<=768}),document.querySelectorAll(".p-footer__block > *").forEach(e=>{e.querySelector(".p-footer__block-head").addEventListener("click",function(){n&&e.classList.toggle("is-open")})})});
